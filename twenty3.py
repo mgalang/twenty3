65529690d8fecbf81087c6f43316f054288785ec
Marc Galang <marchenrygalang@gmail.com>
2014-11-06 08:39:15 +0200

    Add break duration argument and sleep timeout. Add notification when it is time to get back to work

--- twenty3.py
+++ twenty3.py
@@ -5,15 +5,28 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--min', type=int, help="Minutes before break", default="20")
+    parser.add_argument('--min', type=int, help="Timeout before sending alert (minutes)", default="20")
+    parser.add_argument('--duration', type=int, help="Duration of break (seconds)", default="20")
     args = parser.parse_args()
 
-    if not args.min:
-        raise ValueError("Invalid minutes")
+    if not (args.min and args.duration):
+        raise ValueError("Invalid arguments")
 
     while True:
+        # sleep for n minutes
         sleep(args.min*60)
-        Notifier.notify('Time for a break.', title="Reminder")
+
+        # break time
+        Notifier.notify(
+            'Take a break for %d secs' % args.duration,
+            title="Break reminder"
+        )
+
+        # on break
+        sleep(args.duration)
+
+        # back to work
+        Notifier.notify("Back to work", Title="Break reminder")
 
 if __name__ == '__main__':
     try:
